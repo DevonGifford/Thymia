70963a23c3e6ca001c3f010cb6ec099780c7f2c9
DevonGifford <DevonGifford@outlook.com>
2023-09-28 02:32:03 +0200

    Working prototype

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest } from "next";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    return NextResponse.json({ success:  `${req.body.eventName} event logged successfully` }, { status: 200 });
+    return NextResponse.json({ success:  `${req.body} event logged successfully` }, { status: 200 });
   } catch (error) {
     // Handle errors, if any
     return NextResponse.json({ error: "Something went wrong" }, { status: 405 });
